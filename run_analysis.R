8e02afdc9f671c2c1751a06130fbc38c90d8ec42
Jebraith <jebraith@gmail.com>
2015-08-23 14:44:24 -0400

    Include tidydata.txt and modified run_analysis.R script

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,18 @@
-##You should create one R script called run_analysis.R that does the following. 
+##************************************************************************************
+## Created by: Joan Braithwaite
+## Date: 8/22/2015
+## Project: Getting and Cleaning data, Course project
+##
+## Data Source: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+##
+##run_analysis.R - script does the following. 
 ##1.Merges the training and the test sets to create one data set.
 ##2.Extracts only the measurements on the mean and standard deviation for each measurement. 
 ##3.Uses descriptive activity names to name the activities in the data set
 ##4.Appropriately labels the data set with descriptive variable names.
 ##5.From the data set in step 4, creates a second, independent tidy data set 
 ##  with the average of each variable for each activity and each subject.
+##************************************************************************************
 
   
 library(plyr)
@@ -46,6 +54,7 @@ library(knitr)
   
   ##3.Uses descriptive activity names to name the activities in the data set
    activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+
  
   ##4.Label the data set with descriptive variable names.  
     names(Data)<-gsub("^t", "time", names(Data))
